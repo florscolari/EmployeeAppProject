a59a17921221cccbc1a554870d0e1581cd1a03d4
Florencia Scolari <florscolari0@gmail.com>
2025-06-02 15:20:44 +0800

    increment of AddEmployeeFragment. To be continued

--- app/src/main/java/au/edu/kbs/mobiledevelopment/employeeapp/fragments/AddEmployeeFragment.kt
+++ app/src/main/java/au/edu/kbs/mobiledevelopment/employeeapp/fragments/AddEmployeeFragment.kt
@@ -5,30 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import au.edu.kbs.mobiledevelopment.employeeapp.R
+import au.edu.kbs.mobiledevelopment.employeeapp.databinding.FragmentAddEmployeeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class AddEmployeeFragment : Fragment(R.layout.fragment_add_employee), MenuProvider {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddEmployeeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class AddEmployeeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var addEmployeeBinding: FragmentAddEmployeeBinding? = null
+    private val binding get() = addEmployeeBinding!!
+    //to continue from here 1:02:35
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +23,5 @@ class AddEmployeeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add_employee, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddEmployeeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddEmployeeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
