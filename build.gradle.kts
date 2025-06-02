74277a49ec8e93a5aabbc5cb0ac44366f2dc344c
Florencia Scolari <florscolari0@gmail.com>
2025-06-02 17:16:39 +0800

    we have an app running with errors but on the screen

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,16 @@ buildscript {
     }
 
     dependencies {
-        val navVersion = "2.7.5"
+        //val navVersion = "2.7.5"
         classpath(libs.androidx.navigation.safe.args.gradle.plugin)
     }
 }
 
 // KPS instead of KAPT
 plugins {
-    id ("com.android.application") version "8.9.0" apply false
-    id ("com.android.library") version "7.3.1" apply false
-    id ("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    id ("com.android.application") version "8.9.3" apply false
+    id ("com.android.library") version "8.9.3" apply false
+    id ("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
+//id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
