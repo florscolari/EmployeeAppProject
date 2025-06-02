9259d73d25a49327b0fd5e34762151d643ec4eba
Florencia Scolari <florscolari0@gmail.com>
2025-06-02 14:27:46 +0800

    added setupViewModel on Main activity kt

--- app/src/main/java/au/edu/kbs/mobiledevelopment/employeeapp/MainActivity.kt
+++ app/src/main/java/au/edu/kbs/mobiledevelopment/employeeapp/MainActivity.kt
@@ -5,16 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import au.edu.kbs.mobiledevelopment.employeeapp.database.EmployeeDatabase
+import au.edu.kbs.mobiledevelopment.employeeapp.repository.EmployeeRepository
+import au.edu.kbs.mobiledevelopment.employeeapp.viewmodel.EmployeeViewModel
+import au.edu.kbs.mobiledevelopment.employeeapp.viewmodel.EmployeeViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var employeeViewModel: EmployeeViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setupViewModel()
         }
+
+    private fun setupViewModel(){
+        val employeeRepository = EmployeeRepository(EmployeeDatabase(this))
+        val viewModelProviderFactory = EmployeeViewModelFactory(application, employeeRepository)
+        employeeViewModel = ViewModelProvider(this, viewModelProviderFactory)[EmployeeViewModel::class.java]
     }
 }
